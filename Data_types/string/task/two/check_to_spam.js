1ddef1770dfac9ac92ff8d6ebd9d1035e74fd5ad
Bootlegger_Dmitry <61204780+BootleggerHigh@users.noreply.github.com>
2020-05-28 01:46:13 +0300

    Added: task string

---
+++ Data_types/string/task/two/check_to_spam.js
@@ -0,0 +1,16 @@
+/*Напишите функцию checkSpam(str), возвращающую true, если str содержит 'viagra' или 'XXX', а иначе false.
+
+Функция должна быть нечувствительна к регистру:
+
+checkSpam('buy ViAgRA now') == true
+checkSpam('free xxxxx') == true
+checkSpam("innocent rabbit") == false*/
+
+
+alert(checkSpam('text'));
+
+
+function checkSpam(text) {
+    return text.toLowerCase().includes('viagra') ||
+        text.includes('xxx')
+}
