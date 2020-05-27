8d0c274980029bb2649ea5d0533f6c41a8bcbaee
Bootlegger_Dmitry <61204780+BootleggerHigh@users.noreply.github.com>
2020-05-28 01:45:00 +0300

    Added: task json

---
+++ Data_types/json/task/first/object_to_json_json_to_object.js
@@ -0,0 +1,16 @@
+/*Преобразуйте user в JSON, затем прочитайте этот JSON в другую переменную.
+
+let user = {
+  name: "Василий Иванович",
+  age: 35
+};*/
+
+
+
+let user = {
+    name: "Василий Иванович",
+    age: 35
+};
+
+let user_final = JSON.parse(JSON.stringify(user));
+console.log(user_final);
