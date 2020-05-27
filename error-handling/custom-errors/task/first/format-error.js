53e881827faf4ef92cab6103d2b94de47ee6d957
Bootlegger_Dmitry <61204780+BootleggerHigh@users.noreply.github.com>
2020-05-28 01:30:04 +0300

    Added: task for custom-errors

---
+++ error-handling/custom-errors/task/first/format-error.js
@@ -0,0 +1,24 @@
+/*Создайте класс FormatError, который наследует от встроенного класса SyntaxError.
+
+Класс должен поддерживать свойства message, name и stack.*/
+
+
+
+
+
+
+
+class FormatError extends SyntaxError{
+    constructor(message) {
+        super(message);
+        this.name = "FormatError"
+    }
+}
+let err = new FormatError("ошибка форматирования");
+
+alert(err.message);
+alert(err.name);
+alert(err.stack);
+
+alert(err instanceof FormatError);
+alert(err instanceof SyntaxError);
