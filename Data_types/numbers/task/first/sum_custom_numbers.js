e51ff179fd29b53a2dec82f0f398a4d66830d0d5
Bootlegger_Dmitry <61204780+BootleggerHigh@users.noreply.github.com>
2020-05-28 01:46:02 +0300

    Added: task numbers

---
+++ Data_types/numbers/task/first/sum_custom_numbers.js
@@ -0,0 +1,8 @@
+/*Создайте скрипт, который запрашивает ввод двух чисел (используйте prompt)
+и после показывает их сумму
+*/
+
+let first_custom_number = +prompt('Введите первое число', '0');
+let second_custom_number = +prompt('Введите второе число', '0');
+
+alert(first_custom_number + second_custom_number);
