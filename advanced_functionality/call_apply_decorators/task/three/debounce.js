0c421fcd9416dceac1c15d40d819138bd3f6814b
Bootlegger_Dmitry <61204780+BootleggerHigh@users.noreply.github.com>
2020-05-28 01:40:53 +0300

    Added:  task call_apply_decorators

---
+++ advanced_functionality/call_apply_decorators/task/three/debounce.js
@@ -0,0 +1,26 @@
+/*Результатом декоратора debounce(f, ms) должна быть обёртка, которая передаёт вызов f не более одного раза в ms миллисекунд. Другими словами, когда мы вызываем debounce, это гарантирует, что все остальные вызовы будут игнорироваться в течение ms.*/
+
+
+
+function debounce(fun,time)
+{
+    let start_run = false;
+    return function () {
+        if (start_run) return;
+        fun.apply(this,arguments);
+        start_run = true;
+        setTimeout(()=>start_run = false,time);
+    }
+}
+
+
+
+
+let f = debounce(alert, 1000);
+
+f(1); // выполняется немедленно
+f(2); // проигнорирован
+
+setTimeout( () => f(3), 100); // проигнорирован (прошло только 100 мс)
+setTimeout( () => f(4), 1100); // выполняется
+setTimeout( () => f(5), 1500);
